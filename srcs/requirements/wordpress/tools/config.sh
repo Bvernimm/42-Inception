c8f6f2b53200ef9a6cdf47909e918556c61799c1
Bvernimm <bazvernim@outlook.fr>
2023-08-31 12:50:43 +0200

    little fix

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -3,9 +3,8 @@ wp config create	--allow-root \
 					--dbname=$SQL_DATABASE \
 					--dbuser=$SQL_USER \
 					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 \
-					--path='/var/www/wordpress'
-wp core download    --url=$DOMAIN\
+					--dbhost=mariadb:3306 
+wp core install     --url=$DOMAIN\
                     --title=$WP_TITLE\
                     --admin_user=$WP_ADMIN\
                     --admin_password=$WP_ADMIN_PASSWORD\
