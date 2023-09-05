d214934d0978683ec6198374acc85103e38c6900
Bvernimm <bazvernim@outlook.fr>
2023-09-05 12:13:30 +0200

    little fix

--- srcs/requirements/mariadb/tools/MySQL.sh
+++ srcs/requirements/mariadb/tools/MySQL.sh
@@ -7,7 +7,7 @@ sed -i "s/SQL_DATABASE/${SQL_DATABASE}/" /usr/local/bin/initial_db.sql;
 sed -i "s/SQL_USER/${SQL_USER}/" /usr/local/bin/initial_db.sql;
 sed -i "s/SQL_PASSWORD/${SQL_PASSWORD}/" /usr/local/bin/initial_db.sql;
 
-echo "${SQL_DATABASE} est ok avec ${SQL_USER} et ${SQL_PASSWORD}";
+# echo "${SQL_DATABASE} est ok avec ${SQL_USER} et ${SQL_PASSWORD}";
 
 # Lance MySQL
 exec mysqld_safe --init-file=/usr/local/bin/initial_db.sql
