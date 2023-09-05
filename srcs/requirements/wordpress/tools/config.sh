81b8535fbc223c8555e2673a184a83bd7b4f2324
Bvernimm <bazvernim@outlook.fr>
2023-09-05 13:51:57 +0200

    little fix

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -2,26 +2,21 @@
 
 if [ ! -f wp-config.php ]; then
     envsubst < /tools/extra-wp-config.php\
-    | wp config create\
-        --dbname=$WP_DB\
-        --dbuser=$WP_DB_USER\
-        --dbpass=$WP_DB_PASSWORD\
-        --dbhost=mariadb\
-        --allow-root\
-        --extra-php
-    wp core install\
-        --url=$DOMAIN\
-        --title=$WP_TITLE\
-        --admin_user=$WP_ADMIN\
-        --admin_password=$WP_ADMIN_PASSWORD\
-        --admin_email=$WP_ADMIN_EMAIL\
-        --skip-email\
-        --allow-root
-    wp user create\
-        $WP_USER $WP_USER_EMAIL\
-        --role=author\
-        --user_pass=$WP_USER_PASSWORD\
-        --allow-root
+    | wp config create	--allow-root \
+						--dbname=$SQL_DATABASE \
+						--dbuser=$SQL_USER \
+						--dbpass=$SQL_PASSWORD \
+						--dbhost=mariadb:3306 \
+						--path='/var/www/wordpress'
+	wp core install     --url=$DOMAIN\
+ 	                   --title=$WP_TITLE\
+    	                --admin_user=$WP_ADMIN\
+        	            --admin_password=$WP_ADMIN_PASSWORD\
+            	        --allow-root
+	wp user create      $WP_USER\
+        	            --role=author\
+            	        --user_pass=$WP_USER_PASSWORD\
+                	    --allow-root
     # wp theme install teluro --activate --allow-root
     # wp plugin install redis-cache --activate --allow-root
     # wp plugin update --all --allow-root
