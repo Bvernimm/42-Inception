c0ba47f381dcbdb65ec631484e5604d9019390c1
Bvernimm <bazvernim@outlook.fr>
2023-09-05 11:57:19 +0200

    little fix

--- srcs/requirements/mariadb/tools/MySQL.sh
+++ srcs/requirements/mariadb/tools/MySQL.sh
@@ -3,11 +3,11 @@
 echo "MariaDB Setup";
 
 # Modifie les champs pour donner la valeur des variables d'environnement
-sed -i "s/DB_NAME/${DB_NAME}/" /var/www/initial_db.sql;
-sed -i "s/WP_ADMIN_LOGIN/${WP_ADMIN_LOGIN}/" /var/www/initial_db.sql;
-sed -i "s/WP_ADMIN_PASSWORD/${WP_ADMIN_PASSWORD}/" /var/www/initial_db.sql;
+sed -i "s/DB_NAME/${DB_NAME}/" /usr/local/bin/initial_db.sql;
+sed -i "s/WP_ADMIN_LOGIN/${WP_ADMIN_LOGIN}/" /usr/local/bin/initial_db.sql;
+sed -i "s/WP_ADMIN_PASSWORD/${WP_ADMIN_PASSWORD}/" /usr/local/bin/initial_db.sql;
 
 echo "${DB_NAME} est ok avec ${WP_ADMIN_LOGIN} et ${WP_ADMIN_PASSWORD}";
 
 # Lance MySQL
-exec mysqld_safe --init-file=/var/www/initial_db.sql
+exec mysqld_safe --init-file=/usr/local/bin/initial_db.sql
