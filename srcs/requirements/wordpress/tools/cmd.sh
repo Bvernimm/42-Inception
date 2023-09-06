a97c58bf6a91f827d926c2bf2f4cbab5dbd60485
Bvernimm <bazvernim@outlook.fr>
2023-09-06 10:26:27 +0200

    little fix

--- srcs/requirements/wordpress/tools/cmd.sh
+++ srcs/requirements/wordpress/tools/cmd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+chmod +x wp-cli.phar
+cp wp-cli.phar /usr/bin/wp
 sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 chown -R www-data:www-data /var/www/*
 chown -R 755 /var/www/* 
